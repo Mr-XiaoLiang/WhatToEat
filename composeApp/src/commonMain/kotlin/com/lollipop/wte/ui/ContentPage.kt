811cf874d3f8030eea26e0614b9e5e912c58f803
lollipop <liujian594@hotmail.com>
2024-04-27 21:12:28 +0800

    feat: Dialog的效果做好了，接下来是需要处理一下竖屏模式下的Tag样式问题

--- composeApp/src/commonMain/kotlin/com/lollipop/wte/ui/ContentPage.kt
+++ composeApp/src/commonMain/kotlin/com/lollipop/wte/ui/ContentPage.kt
@@ -1,21 +1,29 @@
 package com.lollipop.wte.ui
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
+import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.Dp
@@ -23,18 +31,26 @@ import androidx.compose.ui.unit.dp
 import com.lollipop.wte.Config
 import com.lollipop.wte.DataHelper
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun ContentPage(padding: PaddingValues, dataHelper: DataHelper) {
 
-    var showFlagDialog by remember { mutableStateOf(false) }
+    var showTagDialog by remember { mutableStateOf(false) }
+    val dialogBackground by animateColorAsState(
+        if (showTagDialog) {
+            Color(0x33000000)
+        } else {
+            Color.Transparent
+        }
+    )
 
     MaterialTheme {
         ContentScaffold(
             padding,
             flagPanel = { padding, miniMode ->
                 FlagPanel(padding, miniMode, dataHelper) {
-                    if (!showFlagDialog) {
-                        showFlagDialog = true
+                    if (!showTagDialog) {
+                        showTagDialog = true
                     }
                 }
             },
@@ -42,6 +58,30 @@ fun ContentPage(padding: PaddingValues, dataHelper: DataHelper) {
                 MenuListPanel(padding, dataHelper)
             }
         )
+
+        Column(
+            modifier = Modifier.fillMaxSize().background(dialogBackground)
+        ) {
+            AnimatedVisibility(visible = showTagDialog) {
+                Column(
+                    Modifier.fillMaxSize().padding(8.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Card(
+                        modifier = Modifier.fillMaxWidth().fillMaxHeight(0.8F)
+                    ) {
+                        FlagPanel(padding, false, dataHelper) {}
+                    }
+                    Box(
+                        modifier = Modifier.fillMaxSize().combinedClickable(enabled = true) {
+                            if (showTagDialog) {
+                                showTagDialog = false
+                            }
+                        }
+                    )
+                }
+            }
+        }
 
 //        var showContent by remember { mutableStateOf(false) }
 //
