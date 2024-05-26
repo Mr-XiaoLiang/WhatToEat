61d5f0e7bf7c54ba4ae84cc9fd5d567f813846da
lollipop <liujian594@hotmail.com>
2024-05-26 22:10:02 +0800

    feat: 新的这个在demo能用，但是动画怎么做呢？

--- navigator2/src/main/java/com/lollipop/navigator2/Navigator2.kt
+++ navigator2/src/main/java/com/lollipop/navigator2/Navigator2.kt
@@ -1,13 +1,61 @@
 package com.lollipop.navigator2
 
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.snapshots.SnapshotStateList
+import androidx.compose.runtime.snapshots.SnapshotStateMap
 
-private val routerMap = HashMap<String, PageInfo>()
+fun interface Navigator2 {
+
+    fun navigate(page: String, args: Map<String, Any>?)
+}
+
+fun interface NavIntent {
+
+    fun intent(): Map<String, Any>
+}
 
 class PageInfo(
-    val pagePath: String,
-    val pageContext: @Composable () -> Unit
+    val path: String,
+    val args: Map<String, Any>
 )
 
-interface Navigator2 {
+typealias PageContent = @Composable (PaddingValues, Navigator2, NavIntent) -> Unit
+
+@Composable
+fun NavRoot(
+    paddingValues: PaddingValues,
+    initialPage: String,
+    register: ((String, PageContent) -> Unit) -> Unit,
+) {
+
+    val pageMap = remember {
+        SnapshotStateMap<String, PageContent>()
+    }
+
+    val pageStack = remember { SnapshotStateList<PageInfo>() }
+
+    var isInit by remember { mutableStateOf(false) }
+
+    val navigator2 = Navigator2 { page, args ->
+        pageStack.add(PageInfo(page, args ?: emptyMap()))
+    }
+
+    if (!isInit) {
+        val registerCallback = { key: String, page: PageContent ->
+            pageMap[key] = page
+        }
+        register(registerCallback)
+        navigator2.navigate(initialPage, null)
+        isInit = true
+    }
+
+    pageStack.forEach {
+        pageMap[it.path]?.invoke(paddingValues, navigator2, NavIntent { it.args })
+    }
+
 }
